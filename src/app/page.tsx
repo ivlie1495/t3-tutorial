8b6f6d1c3d315a5ae9ce6893d7d415d20ede377d
ivanlie <ivan.lie@meditap.id>
2024-04-19 16:22:07 +0700

    add auth in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-export default async function HomePage() {
+const Images = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4 p-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt={image.name} />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default async function HomePage() {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4 p-4">
-        {images.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt={image.name} />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full p-4 text-2xl">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
