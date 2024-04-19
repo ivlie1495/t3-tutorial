c78f455cd62aa3d98b5d8fe5e82eb40c219f18c4
ivanlie <ivan.lie@meditap.id>
2024-04-19 14:39:39 +0700

    link database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { db } from "~/server/db";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4 p-4">
-        {posts.map((post) => (
-          <div key={post.id} className="flex flex-col gap-4 p-4">
-            <p className="text-lg">{post.name}</p>
+        {images.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={image.name} />
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
