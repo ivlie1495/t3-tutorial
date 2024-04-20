fbf0922bd64cfe3bd4b4be2a361d4d7b8d1b86a3
ivanlie <ivan.lie@meditap.id>
2024-04-20 09:59:57 +0700

    cleanup on modal

--- src/components/image-full-view.tsx
+++ src/components/image-full-view.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 import { getImage } from "~/server/queries";
 
 export default async function ImageFullView({ id }: { id: number }) {
   const image = await getImage(id);
 
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="relative flex flex-1 items-center justify-center">
+        <Image
+          src={image.url}
+          alt={image.name}
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-col">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
