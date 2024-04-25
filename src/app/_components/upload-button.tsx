69291fa44de8d3297acd53e8835d499919945c57
ivanlie <ivan.lie@meditap.id>
2024-04-25 18:26:24 +0700

    upload event capture

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { usePostHog } from "posthog-js/react";
 import { toast } from "sonner";
 
 import { useUploadThing } from "~/utils/uploadthing";
@@ -50,8 +51,11 @@ const UploadSVG = () => (
 
 const SimpleUploadButton = () => {
   const router = useRouter();
+  const posthog = usePostHog();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin: () => {
+      posthog.capture("upload_begin");
       toast("Uploading...", {
         id: "upload-begin",
       });
